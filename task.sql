23f7792092179b4983d979eb7ac400bf22073af4
Olena Nosenko <nosenko.alena@gmail.com>
2026-09-03 11:53:08 +0300

    added functionality for database normalization

--- task.sql
+++ task.sql
@@ -4,30 +4,42 @@ CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Countries (
-    ID INT,
+    ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE ProductInventory (
-    ID INT,
-    ProductName VARCHAR(50),
-    WarehouseAmount INT,
+CREATE TABLE Warehouse (
+    ID INT AUTO_INCREMENT,
     WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
+    WarehouseAmount INT,
+    WarehouseAddress VARCHAR(100), 
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE ProductInventory (
+    ID INT AUTO_INCREMENT,
+    ProductName VARCHAR(50),
     CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE CASCADE,
+    WarehouseID INT,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouse(ID) ON DELETE CASCADE,
     PRIMARY KEY (ID)
 );
 
 -- Populate test data
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
+INSERT INTO Countries (Name)
+	VALUES ('Country1');
+INSERT INTO Countries (Name)
+	VALUES ('Country2');
+
+INSERT INTO Warehouse (WarehouseName, WarehouseAmount, WarehouseAddress)
+    VALUES('ATB', 1000, 'Ukraine, Dnipro, Naberegna Zavodska 93');
+INSERT INTO Warehouse (WarehouseName, WarehouseAmount, WarehouseAddress)
+    VALUES('Silpo', 5000, 'Latviya, Riga, Prospect Mega 17');
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO ProductInventory (ProductName, CountryID, WarehouseID)
+	VALUES ('AwersomeProduct', 1, 1);
+INSERT INTO ProductInventory (ProductName, CountryID, WarehouseID)
+	VALUES ('AwersomeProduct', 2, 2);
