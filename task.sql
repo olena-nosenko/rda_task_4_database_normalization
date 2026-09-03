ae135bb5e1b898aca69c945664646f9f07de364d
Olena Nosenko <nosenko.alena@gmail.com>
2026-09-03 12:18:47 +0300

    updated functionality for database normalization according comments

--- task.sql
+++ task.sql
@@ -11,19 +11,26 @@ CREATE TABLE Countries (
 
 CREATE TABLE Warehouse (
     ID INT AUTO_INCREMENT,
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE CASCADE,
     WarehouseName VARCHAR(50),
-    WarehouseAmount INT,
     WarehouseAddress VARCHAR(100), 
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Products (
+    ID INT AUTO_INCREMENT,
+    ProductName VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE ProductInventory (
     ID INT AUTO_INCREMENT,
-    ProductName VARCHAR(50),
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE CASCADE,
+    ProductID INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE CASCADE,
     WarehouseID INT,
     FOREIGN KEY (WarehouseID) REFERENCES Warehouse(ID) ON DELETE CASCADE,
+    WarehouseAmount INT,
     PRIMARY KEY (ID)
 );
 
@@ -34,12 +41,17 @@ INSERT INTO Countries (Name)
 INSERT INTO Countries (Name)
 	VALUES ('Country2');
 
-INSERT INTO Warehouse (WarehouseName, WarehouseAmount, WarehouseAddress)
-    VALUES('ATB', 1000, 'Ukraine, Dnipro, Naberegna Zavodska 93');
-INSERT INTO Warehouse (WarehouseName, WarehouseAmount, WarehouseAddress)
-    VALUES('Silpo', 5000, 'Latviya, Riga, Prospect Mega 17');
+INSERT INTO Warehouse (CountryID, WarehouseName, WarehouseAddress)
+    VALUES(1, 'ATB', 'Ukraine, Dnipro, Naberegna Zavodska 93');
+INSERT INTO Warehouse (CountryID, WarehouseName, WarehouseAddress)
+    VALUES(2, 'Silpo', 'Latviya, Riga, Prospect Mega 17');
+
+INSERT INTO Products (ProductName)
+VALUES ('Bread');
+INSERT INTO Products (ProductName)
+VALUES ('Milk');
     
-INSERT INTO ProductInventory (ProductName, CountryID, WarehouseID)
-	VALUES ('AwersomeProduct', 1, 1);
-INSERT INTO ProductInventory (ProductName, CountryID, WarehouseID)
-	VALUES ('AwersomeProduct', 2, 2);
+INSERT INTO ProductInventory (ProductID, WarehouseID, WarehouseAmount)
+	VALUES (1, 1, 100);
+INSERT INTO ProductInventory (ProductID, WarehouseID, WarehouseAmount)
+	VALUES (2, 2, 500);
